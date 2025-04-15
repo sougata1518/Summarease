418024e8da61adaaab0c5cb77237370a31f28e0c
Soumyakhi <soumyajitwwe@gmail.com>
2025-04-15 17:20:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allow connections from any IP address on the local network
+    port: 5173
+  },
 })
